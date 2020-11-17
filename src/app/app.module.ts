5fa74d0dfd7ac32f1029bc15d517d72a97d5b884
Gerardo Espinosa <ivanmtze96@gmail.com>
2020-11-16 23:34:59 -0600

    add ui -red #23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AdditionComponent } from './addition/addition.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdditionComponent
+    AdditionComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule
